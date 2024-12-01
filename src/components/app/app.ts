0e3434f2eff7bdd5e8cc5369b83642b8bd3976e1
Antiart <r2d2rom@mail.ru>
2024-12-02 04:54:53 +0500

    А ну ка

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -1,16 +1,32 @@
 import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
 
+// Определяем интерфейсы для контроллера и представления
+interface AppControllerInterface {
+    getNews(event: Event, callback: (data: unknown) => void): void;
+    getSources(callback: (data: unknown) => void): void;
+}
+
+interface AppViewInterface {
+    drawNews(data: unknown): void;
+    drawSources(data: unknown): void;
+}
+
+// Определяем класс App с использованием интерфейсов
 class App {
+    private controller: AppControllerInterface;
+    private view: AppViewInterface;
+
     constructor() {
         this.controller = new AppController();
         this.view = new AppView();
     }
 
-    start() {
+    start(): void {
         document
             .querySelector('.sources')
             .addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
+
         this.controller.getSources((data) => this.view.drawSources(data));
     }
 }
