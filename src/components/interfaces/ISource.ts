64bd4fbfaccc7fb912857426a9ca711cc4b69966
Antiart <r2d2rom@mail.ru>
2024-12-02 07:36:02 +0500

    Реализована адаптивность дизайна

--- src/components/interfaces/ISource.ts
+++ src/components/interfaces/ISource.ts
@@ -1,11 +1,11 @@
 import { IDocument } from './IDocument';
 
 export interface ISource extends IDocument {
-  id: string;
-  category: string;
-  country: string;
-  description: string;
-  language: string;
-  name: string;
-  url: string;
+    id: string;
+    category: string;
+    country: string;
+    description: string;
+    language: string;
+    name: string;
+    url: string;
 }
--- src/components/interfaces/TResponsePortal.ts
+++ src/components/interfaces/TResponsePortal.ts
@@ -2,15 +2,12 @@ import { IArticle } from './IArticle';
 import { ISource } from './ISource';
 
 export interface IResponsePortal {
-  status: string;
-  totalResults: number;
-  articles: IArticle[] | [];
-  sources: ISource[];
+    status: string;
+    totalResults: number;
+    articles: IArticle[] | [];
+    sources: ISource[];
 }
 
-export type DataForNews = Pick<
-  IResponsePortal,
-  'status' | 'totalResults' | 'articles'
->;
+export type DataForNews = Pick<IResponsePortal, 'status' | 'totalResults' | 'articles'>;
 
 export type DataForSource = Pick<IResponsePortal, 'status' | 'sources'>;
